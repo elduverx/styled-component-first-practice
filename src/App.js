c4e40bce85a86b2b761823a0cac1b6eca67c797c
elduverx <duverwild@gmail.com>
2022-01-05 04:56:58 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import styled,{keyframes} from 'styled-components'
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`
+const Content = styled.div`
+padding:20px 25px`
+
+const Button = styled.button`
+transition: box-shadow 0.2s ease;
+  background-color: ${props => props.primary ? 'red' : 'white'};
+  color: ${props => props.primary ? 'white' : 'red'};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+  &:hover{
+    box-shadow: 1px 2px 5px rgb(0,0,0,0.3);
+
+  }
+  &.secondary{
+    background-color: blue;
+    border: solid 2px blue; 
+  }
+  .info{
+    font-size:28px;
+
+  }
+
+  `
+
+const BlockButton= styled(Button)`
+  width:100%;
+  font-size:24px;
+
+`
+const Link = ({classname, ...props})=>{
+  return <a className={classname}{...props}></a>
+}
+
+const StyledLink = styled(Link)`
+color: blue;
+
+`
+
+const Input = styled.input.attrs(props => ({
+  type: 'text',
+  color: props.color || 'red'
+}))`
+  font-size: 20px;
+  border: 1px solid red;
+  color: ${props=> props.color};
+`
+
+const Password = styled(Input).attrs({
+  type: 'password'
+})``
+//para animar con keyframes
+const girar = keyframes`
+from{
+  transform: rotate(0deg)
+}
+to{
+  transform: rotate(360deg)
+}
+`
+const Rotar = styled.div`
+display: inline-block;
+animation: ${girar} 2s linear infinite;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Content>
+
+      <P>HolimP</P>
+      <Button>Send</Button>
+      <Button primary>Send<p className='info'>Info</p></Button>
+      <Button primary className='secondary'>Send</Button>
+      <BlockButton primary as="a" href="#">Send</BlockButton>
+      <Link>Link</Link>
+      <StyledLink>Link con stilo</StyledLink>
+      <Input/>
+      <Password/>
+      <br/>
+      <Rotar>Im spinning</Rotar>
+    </Content>
+      );
 }
 
 export default App;
